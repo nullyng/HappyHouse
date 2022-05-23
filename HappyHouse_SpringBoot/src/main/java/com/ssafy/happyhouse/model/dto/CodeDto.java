c15e3dc1048ca8e1ffa4ac8263e01c826f021bf4
nullyng <young315@knu.ac.kr>
2022-05-23 21:42:50 +0900

    Edit: CodeDto, CodeMapper.java lat, lng도 얻어오도록 Dto 및 쿼리문 수정

--- HappyHouse_SpringBoot/src/main/java/com/ssafy/happyhouse/model/dto/CodeDto.java
+++ HappyHouse_SpringBoot/src/main/java/com/ssafy/happyhouse/model/dto/CodeDto.java
@@ -3,6 +3,8 @@ package com.ssafy.happyhouse.model.dto;
 public class CodeDto {
     private String code;
     private String name;
+    private String lat;
+    private String lng;
 
     public CodeDto(String code, String name) {
         this.code = code;
@@ -23,5 +25,21 @@ public class CodeDto {
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+    
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
     }
 }
