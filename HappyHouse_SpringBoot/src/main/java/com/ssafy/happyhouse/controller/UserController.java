4144d2cb72afa106ad47b5050f69b16e8d90f020
Inhu Jo <inhudev@gmail.com>
2022-05-23 22:13:23 +0900

    Edit: spring boot userController

--- HappyHouse_SpringBoot/src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ HappyHouse_SpringBoot/src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -1,17 +1,29 @@
 package com.ssafy.happyhouse.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.ssafy.happyhouse.model.dto.UpdateUserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.happyhouse.model.dto.UpdateUserDto;
 import com.ssafy.happyhouse.model.dto.UserDto;
+import com.ssafy.happyhouse.service.JwtServiceImpl;
 import com.ssafy.happyhouse.service.UserService;
 
 @RestController
@@ -23,6 +35,9 @@ public class UserController{
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private JwtServiceImpl jwtService;
 
 	@GetMapping("/")
 	public ResponseEntity<?> getUser(HttpSession session){
@@ -40,12 +55,26 @@ public class UserController{
 
 	@GetMapping("/login")
 	public ResponseEntity<?> login(@RequestParam("id") String id, @RequestParam("pwd") String pwd, HttpSession session, HttpServletResponse response){
-		UserDto user = userService.LoginUserDto(id, pwd);
-		if(user != null){
-			session.setAttribute("user", user);
-			return new ResponseEntity<>(session.getId(), HttpStatus.OK);
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			UserDto user = userService.LoginUserDto(id, pwd);
+			if(user != null){
+				session.setAttribute("user", user);
+				String token = jwtService.create("userid", user.getUserId(), "access-token");
+				resultMap.put("access-token", token);
+				resultMap.put("message", "success");
+				status = HttpStatus.ACCEPTED;
+			} else {
+				resultMap.put("message", "fail");
+				status = HttpStatus.ACCEPTED;
+			}
+		} catch (Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		
+		return new ResponseEntity<>(resultMap, status);
 	}
 
 	@GetMapping("/logout")
